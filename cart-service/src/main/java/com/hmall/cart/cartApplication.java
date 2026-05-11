c1ba815c5b82147c9427a7af17ca6fc454843d4c
xinleizhang <xinleiz328@gmail.com>
2026-05-11 16:23:22 +0800

    黑马商城项目拆分-nacos使用openFeign独立分装为hm-api

--- cart-service/src/main/java/com/hmall/cart/cartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/cartApplication.java
@@ -1,5 +1,7 @@
 package com.hmall.cart;
 
+import com.hmall.api.client.ItemClient;
+import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(clients = {ItemClient.class},defaultConfiguration = DefaultFeignConfig.class)
 public class cartApplication {
     public static void main(String[] args) {
         SpringApplication.run(cartApplication.class, args);
--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -1,12 +1,12 @@
-package com.hmall.cart.client;
+package com.hmall.api.client;
 
-import com.hmall.cart.domain.dto.ItemDTO;
+
+import com.hmall.api.dto.ItemDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @FeignClient("item-service")
