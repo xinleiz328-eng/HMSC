eaf022914077db6144637b02a3e97ea103f63818
xinleizhang <xinleiz328@gmail.com>
2026-05-11 21:56:46 +0800

    黑马商城项目拆分pay-Service完成

---
+++ pay-service/src/main/java/com/hmall/pay/payApplication.java
@@ -0,0 +1,17 @@
+package com.hmall.pay;
+
+import com.hmall.api.config.DefaultFeignConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@MapperScan("com.hmall.pay.mapper")
+@SpringBootApplication
+@EnableFeignClients(basePackages = {"com.hmall.api.client"}, defaultConfiguration = DefaultFeignConfig.class)
+public class payApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(payApplication.class, args);
+    }
+
+}
