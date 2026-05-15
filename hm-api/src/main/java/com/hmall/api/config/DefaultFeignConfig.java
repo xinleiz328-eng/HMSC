af1bd7dcc63dbd448fa77e153bc700b2a4c61d74
xinleizhang <xinleiz328@gmail.com>
2026-05-15 11:22:52 +0800

    黑马商城nacos实现共享配置

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,12 +1,28 @@
 package com.hmall.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 
 public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLoggerLevel(){
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor userInRequestHeaderInterceptor(){
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                Long userId = UserContext.getUser();
+                if(userId != null){
+                    requestTemplate.header("user-info", userId.toString());
+                }
+            }
+        };
     }
 
 }
